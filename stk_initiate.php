3395919d955df19d3b9d7d70e17ba8c83be3e718
ayubsimbaa53 <102029130+ayubsimbaa53@users.noreply.github.com>
2022-11-20 19:52:18 +0300

    Update stk_initiate.php

--- stk_initiate.php
+++ stk_initiate.php
@@ -5,8 +5,9 @@ if(isset($_POST['submit'])){
   date_default_timezone_set('Africa/Nairobi');
 
   # access token
-  $consumerKey = 'nk16Y74eSbTaGQgc9WF8j6FigApqOMWr'; //Fill with your app Consumer Key
-  $consumerSecret = '40fD1vRXCq90XFaU'; // Fill with your app Secret
+  $consumerKey = 'ANIhJ4veofmptY9HwL4SOC595nFRpkjJ'; //Fill with your app Consumer Key
+  $consumerSecret = 'FrAGUHoG4Ozmbkvs
+  '; // Fill with your app Secret
 
   # define the variales
   # provide the following details, this part is found on your test credentials on the developer account
